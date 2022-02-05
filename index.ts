aa84d9e593c00c8046856f51d0942433c97d0f34
Namoop <theo1cap@gmail.com>
2022-02-05 15:06:10 -0800

    1 file modified

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
 const app: HTMLElement = document.getElementById('app');
 
 import { cnv, draw, sprites } from './setup';
-import { Sprite } from "./Sprite.class"
+import { Sprite } from './Sprite.class';
 
 app.appendChild(cnv);
 const pen = cnv.getContext('2d');
 
-let test = new Sprite("bob.png", sprites)
-  .move(140, 100)
-  .setSize(100)
+let test = new Sprite('bob.png', sprites).move(140, 100).setSize(100);
 
-function run() {test.direction++}
+function run() {
+  test.direction++;
+}
 /**
  * Options that can change how the game runs
  * - Gamespeed: Time between ticks (milliseconds)
@@ -28,9 +28,36 @@ function loop(): void {
   cnv.height = 400 * runOptions.scale;
   run();
   draw();
+  [frame, nextframe] = runAsync([
+    frame + 1,
+    new Promise(function (resolve) {
+      runAsync = resolve;
+    }),
+  ]);
   if (!runOptions.stop) {
     if (runOptions.gamespeed > 0) window.requestAnimationFrame(loop);
     else setTimeout(window.requestAnimationFrame, runOptions.gamespeed, loop);
   }
 }
+let frame = 0;
+let runAsync: Function;
+let nextframe = new Promise(function (resolve) {
+  runAsync = resolve;
+});
 loop();
+
+setTimeout(toodlyoo, 500);
+
+async function toodlyoo() {
+  //let myframe;
+  //[myframe, nextFrame] =
+  await nextframe;
+  console.log(frame);
+
+  while (frame < 55) {
+    //[myframe, nextFrame] =
+    await nextFrame;
+    //if (myframe < 50) console.log(myframe);
+    console.log(frame);
+  }
+}
